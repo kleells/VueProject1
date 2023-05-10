36762aa1883c2df11247f5bba88406448785f5bd
Katie Eells <katieeells@Katies-MBP.lan>
2023-05-09 20:48:46 -0400

    built a dropdown, checkbox and radio button

--- script.js
+++ script.js
@@ -228,6 +228,14 @@ Vue.createApp({
     return {
       inputText: "",
       textAreaContent: "",
+      selected: "Search",
+      options: [
+        { text: "From a search engine", value: "Search"},
+        { text: "From a friend", value: "Friend"},
+        { text: "Other", value: "Other"}
+      ],
+      checkedOptions: [],
+      radioButtonPicked: ""
     }
   }
 }).mount("#form")
